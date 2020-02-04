424a6873e3857e06538dbb9d08f05a3de40ec53d
Your Name <you@example.com>
2020-02-04 08:30:55 +0800

    first commit

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test{
+	public static void main(String args[]){
+		System.out.println("WOOOOOOOOOOOOOOWOWOWOWOWOWWO");
+		System.out.println("SHHHHHHZZZZZZZZZZZZZ");
+	}
+	
+	public void sayHello(){
+		System.out.println("Hi");
+	}
+	
+	public int addNum(int a, int b){
+		retrun a + b;
+	}
+}
